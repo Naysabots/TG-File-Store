7e5d2667c191051d9bae4a38467a5380d22e2ba3
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-03 20:05:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ if OWNER_ID not in AUTH_USERS:
 MONGODB_URL = os.environ.get("MONGODB_URL", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+SESSION_NAME = os.environ.get("SESSION_NAME", "")
