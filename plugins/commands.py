86b65adccf138d2d4f3bf5f3716d4657ef0085aa
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-03 21:34:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,18 @@ async def start(c, m, cb=False):
 
     owner = await c.get_users(int(OWNER_ID))
     owner_username = owner.username if owner.username else 'Ns_bot_updates'
-    await add_user_to_database(user_id)
+    if cmd.from_user.id in BANNED_USERS:
+        await cmd.reply_text("Sorry, You are banned.")
+        return
+    usr_cmd = cmd.text.split("_")[-1]
+    if usr_cmd == "/start":
+        chat_id = cmd.from_user.id
+        if not await db.is_user_exist(chat_id):
+            await db.add_user(chat_id)
+            await bot.send_message(
+                LOG_CHANNEL,
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!"
+            )
     # start text
     text = f"""Hey! {m.from_user.mention(style='md')}
 
@@ -98,7 +109,18 @@ async def start(c, m, cb=False):
 
 @Client.on_message(filters.command('me') & filters.incoming & filters.private)
 async def me(c, m):
-    await add_user_to_database(user_id)
+    if cmd.from_user.id in BANNED_USERS:
+        await cmd.reply_text("Sorry, You are banned.")
+        return
+    usr_cmd = cmd.text.split("_")[-1]
+    if usr_cmd == "/me":
+        chat_id = cmd.from_user.id
+        if not await db.is_user_exist(chat_id):
+            await db.add_user(chat_id)
+            await bot.send_message(
+                LOG_CHANNEL,
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!"
+            )
     """ This will be sent when /me command was used"""
 
     me = await c.get_users(m.from_user.id)
@@ -117,7 +139,18 @@ async def me(c, m):
 
 @Client.on_message(filters.command('batch') & filters.private & filters.incoming)
 async def batch(c, m):
-    await add_user_to_database(user_id)
+    if cmd.from_user.id in BANNED_USERS:
+        await cmd.reply_text("Sorry, You are banned.")
+        return
+    usr_cmd = cmd.text.split("_")[-1]
+    if usr_cmd == "/batch":
+        chat_id = cmd.from_user.id
+        if not await db.is_user_exist(chat_id):
+            await db.add_user(chat_id)
+            await bot.send_message(
+                LOG_CHANNEL,
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!"
+            )
     """ This is for batch command"""
 
     BATCH.append(m.from_user.id)
