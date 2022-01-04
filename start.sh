3e04def7eb9f1f5ab7633feeba28f3a6c542cbb9
DKOWNER <94420774+DKOWNER@users.noreply.github.com>
2022-01-04 07:51:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/DKOWNER/TG-File-Store /TG-File-Store
 cd /TG-File-Store
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @Tellybots"
-python3 main.py
+python3 bot.py
