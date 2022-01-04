4e9a2c6a293196c8e65d8948c506600f745de08d
DKOWNER <94420774+DKOWNER@users.noreply.github.com>
2022-01-04 08:02:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID"))
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
 
-MONGODB_URL = os.environ.get("MONGODB_URL", "")
+MONGODB_URL = os.environ.get("DATABASE_URL")
 LOG_CHANNEL = int(os.environ.get("DB_CHANNEL_ID", "-100"))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
 SESSION_NAME = os.environ.get("BOT_USERNAME", "")
