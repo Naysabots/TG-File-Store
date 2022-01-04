c36552a37c1b08c93f59800976cfea68b97ef203
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-04 11:34:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/DKOWNER/TG-File-Store /TG-File-Store
+git clone https://github.com/NaysaBots/TG-File-Store /TG-File-Store
 cd /TG-File-Store
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @Tellybots"
